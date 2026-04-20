696b10ecd9399d21158d5fbf52192ddb52c9ad84
DSChakranarayan <dikshantsc@outlook.in>
2026-04-20 19:42:35 +0530

    New Files

--- income.php
+++ income.php
@@ -4,5 +4,33 @@
 <?php include("navbar.php"); ?>
 
 
+<div class="row">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+                <table class="table">
+                    <tr>
+                        <th>Sr.</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                    <tr>
+                        <td>Sr.</td>
+                        <td>Name</td>
+                        <td>Price</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php include("footer.php"); ?>
